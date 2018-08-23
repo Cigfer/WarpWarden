76c9571e7c23e1f3acaa9902299e8b9e9f832509
ruslank <rkay94@gmail.com>
2018-08-23 16:21:02 -0400

    Add shield spinner and tank prefab

---
+++ Earth Defense/Assets/Scripts/ShieldSpinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldSpinner : MonoBehaviour
+{
+    public float SpinRate = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.up, SpinRate * Time.deltaTime);
+	}
+}
