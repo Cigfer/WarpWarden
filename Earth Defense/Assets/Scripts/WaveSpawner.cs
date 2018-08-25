a4029aef439db53c0828468a9146bceb80024a36
ruslank <rkay94@gmail.com>
2018-08-25 01:35:00 -0400

    waves

--- Earth Defense/Assets/Scripts/WaveSpawner.cs
+++ Earth Defense/Assets/Scripts/WaveSpawner.cs
@@ -8,22 +8,39 @@ public class WaveSpawner : MonoBehaviour
     public GameObject PrefabToSpawn = null;
     public GameObject GoalToGive = null;
     public float DelayOffset = 0f;
+    private float StartTimeOffset = 0f;
 
-    public int MaxToSpawn = 10;
+    
+    public int TotalToSpawn = 100;
 
     public float TimeBetweenSpawn = 5f;
     private float LastTimeSpawned = 0f;
+
+    private bool first = true;
+    private float StartTime = 0f;
 
     private List<GameObject> SpawnedObjects;
 	// Use this for initialization
 	void Start () {
         SpawnedObjects = new List<GameObject>();
-        
-	}
+
+    }
+
+    void OnEnable()
+    {
+        //DelayOffset += Time.time;
+        //LastTimeSpawned = Time.time;
+        StartTimeOffset = Time.time;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-	    if (SpawnedObjects.Count < MaxToSpawn && LastTimeSpawned + TimeBetweenSpawn < Time.time - DelayOffset)
+	    if (first && 0 < Time.time - (DelayOffset + StartTimeOffset))
+	    {
+	        SpawnEnemyType1();
+	        first = false;
+	    }
+	    if ((LastTimeSpawned + TimeBetweenSpawn < Time.time - (DelayOffset + StartTimeOffset)) && TotalToSpawn > 0)
 	    {
 	        SpawnEnemyType1();
 	    }
@@ -33,10 +50,15 @@ public class WaveSpawner : MonoBehaviour
     {
         Vector3 spawnPos = transform.position;
         spawnPos.y += (Random.value * 4f) - 2;
-        GameObject spawned = Instantiate(PrefabToSpawn, spawnPos, Quaternion.identity);
+        GameObject spawned = Instantiate(PrefabToSpawn, spawnPos, transform.rotation);
         SpawnedObjects.Add(spawned);
-        var setter = spawned.GetComponent<AIDestinationSetter>();
+        var setter = spawned.GetComponentInChildren<AIDestinationSetter>();
         setter.target = GoalToGive.transform;
         LastTimeSpawned = Time.time;
+        TotalToSpawn--;
+        if (TotalToSpawn == 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
---
+++ Earth Defense/Assets/WaveManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public int WaveNumber = 0;
+    public List<float> WaveStartTimes;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        while (WaveStartTimes.Count < transform.childCount)
+        {
+            WaveStartTimes.Add(999999);
+        }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (WaveStartTimes[i] < Time.time)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+                WaveNumber++;
+            }
+        }
+	}
+}
